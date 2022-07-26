21ac093d77fbfddfcfc97cc486612eedb4ea248b
Cam Mac Leod <camden.macleod@pm.me>
2022-07-26 10:48:23 -0400

    Initial commit

---
+++ app/src/main/java/com/example/testapp/FirstFragment.kt
@@ -0,0 +1,73 @@
+package com.example.testapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.example.testapp.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Binding for subtract button
+        binding.subtractButton.setOnClickListener {
+            countMe(true, view)
+        }
+
+        // Binding for add button
+        binding.addButton.setOnClickListener {
+            countMe(false, view)
+        }
+
+        // Binding for toast button
+        binding.toastButton.setOnClickListener {
+            val textView = view.findViewById<TextView>(R.id.textview_first)
+            val myToast = Toast.makeText(context, textView.text.toString(), Toast.LENGTH_SHORT)
+            myToast.show()
+        }
+    }
+
+    private fun countMe(subtract: Boolean, view: View) {
+        val textView = view.findViewById<TextView>(R.id.textview_first)
+        var count = textView.text.toString().toInt()
+
+        if (subtract) {
+            count--
+        }
+        else {
+            count++
+        }
+
+        textView.text = count.toString()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
